cd1f92f48a7d29a01c63d01c5b90560aa2a494da
origin <jarco.aleksandra@gmail.com>
2026-09-22 10:41:46 +0200

    added chembl ingestion with spark

---
+++ src/ingest_chembl_spark.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+def main():
+    spark =(
+        SparkSession.builder
+        .appName("ChemblIngestion")
+        .master("local[*]")
+        .getOrCreate()
+    )
+    df = spark.read.option("multiLine", True).json(
+        "data/transformed/page_1.json"
+    )
+    print("Number of rows: ", df.count())
+    df.printSchema()
+    df.show(5, truncate=False)
+
+    spark.stop()
+
+if __name__ == "__main__":
+    main()
